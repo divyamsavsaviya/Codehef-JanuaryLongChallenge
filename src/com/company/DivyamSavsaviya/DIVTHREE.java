4e774c8a45a39f5d481d5518b4d6b268271c88a2
divyam <divyam29102001@gmail.com>
2021-03-17 23:20:19 +0530

    Initial commit 1) adding DIVTHREE.java

---
+++ src/com/company/DivyamSavsaviya/DIVTHREE.java
@@ -0,0 +1,21 @@
+package com.company.DivyamSavsaviya;
+
+import java.util.Scanner;
+
+public class DIVTHREE {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        int t = scanner.nextInt();
+        while(t-- > 0){
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
+            int d = scanner.nextInt();
+            int totalProblem = 0;
+            for (int i = 0 ; i < n ; i++){
+                totalProblem += scanner.nextInt();
+            }
+            System.out.println(Math.min(d, totalProblem / k));
+        }
+
+    }
+}
